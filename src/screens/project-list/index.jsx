0674a9d72a7b1bbefdfcf30656388ffb1904c597
杨梓皓 <835627827@qq.com>
2021-04-15 21:05:42 +0800

    chore 实现防抖效果

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react"
 import { List } from "./list"
-import { SearchPanel } from "./search-panel"
+import { SearchPanel } from "screens/project-list/search-panel"
 import React from 'react';
-import {cleanObject} from "../../utils";
+import {cleanObject} from "utils";
 import * as qs from "qs";
+import {useMount} from "utils";
+import {useDebounce} from "../../utils";
 
 const apiUrl = process.env.REACT_APP_API_URL
 export const ProjctListScreen = () => {
@@ -12,6 +14,8 @@ export const ProjctListScreen = () => {
         name: '',
         personId: ''
     })
+    const debouncedParam = useDebounce(param,2000)
+
     const [list, setList] = useState([])
     useEffect(() => {
         fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async resPonse => {
@@ -19,14 +23,21 @@ export const ProjctListScreen = () => {
                 setList(await resPonse.json())
             }
         })
-    }, [param])
-    useEffect(() => {
-        fetch(`${apiUrl}/users`).then(async resPonse => {
-            if (resPonse.ok) {
-                setUsers(await resPonse.json())
-            }
+    }, [debouncedParam])// eslint-disable-line react-hooks/exhaustive-deps
+    // useEffect(() => {
+    //     fetch(`${apiUrl}/users`).then(async resPonse => {
+    //         if (resPonse.ok) {
+    //             setUsers(await resPonse.json())
+    //         }
+    //     })
+    // }, [])
+    useMount(() => {
+            fetch(`${apiUrl}/users`).then(async resPonse => {
+                if (resPonse.ok) {
+                    setUsers(await resPonse.json())
+                }
+            })
         })
-    }, [])
     return <div>
         <SearchPanel users={users} param={param} setParam={setParam} />
         <List list={list} users={users} />
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,6 @@
+import {useEffect, useState} from "react";
+
+
 export const isFalsy = (value) => value === 0 ?true :  !!value
 
 
@@ -12,3 +15,48 @@ export const cleanObject = (object) =>{
     })
     return result;
 }
+
+export const useMount = (callback) =>{
+    useEffect(()=>{
+        callback()
+    },[])// eslint-disable-line react-hooks/exhaustive-deps
+}
+
+export  const  useDebounce = (value,delay) =>{
+    // 这是一个内部得变量进入这个函数不会马上更新debouncedValue得值；
+    const [debouncedValue,setDebouncedValue] = useState(value);
+    // 每次在value变化得时候会设置一个定时器
+    useEffect(()=>{
+        const timeout = setTimeout(()=>{
+            // 执行完setTimeout设置debouncedvalue得值
+            setDebouncedValue(value)
+        },delay)
+        // 每次在useEffect处理完以后在运行
+        return ()=> clearTimeout(timeout)
+    },[value,delay])
+    return debouncedValue
+}
+// export const useDebounce = (value,delay)=>{
+//     let timeout
+//     return ()=>{
+//         if(timeout){
+//          clearTimeout(timeout)
+//         }
+//         timeout = setTimeout(()=>{
+//             return value
+//         },delay)
+//     }
+// }
+
+// export const debounce = (func , delay)=>{
+//     let timeout;
+//     return ()=>{
+//         if(timeout){
+//             clearTimeout(timeout)
+//         }
+//         timeout = setTimeout (()=>{
+//                 func()
+//         },delay)
+//     }
+// }
+// const log = debounce(()=>{console.log("call")},5000)

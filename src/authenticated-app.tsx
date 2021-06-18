1776ecd1cbe230ac07710f62e1a91ac50304ae0e
835627827 <835627827@qq.com>
2021-06-18 15:27:47 +0800

    style:用定义row组件布局页面

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,11 +1,49 @@
-import { useAuth } from "context/auth-context"
-import { ProjectListScreen } from "screens/project-list"
+import styled from "@emotion/styled";
+import { Row } from "components/lib";
+import { useAuth } from "context/auth-context";
+import { ProjectListScreen } from "screens/project-list";
 
-export const AnthenticatedApp = () =>{
-    const  {logout,user}=useAuth()
-    return <div>
-        <div>{user?.name}</div>
-        <button onClick={()=>{logout()}}>登出</button>
-        <ProjectListScreen/>
-    </div>
-}
+export const AnthenticatedApp = () => {
+  const { logout } = useAuth();
+  return (
+    <Container>
+      <Header between={true}>
+        <HeaderLeft gap={true}>
+          <h3>Logo</h3>
+          <h3>项目</h3>
+          <h3>用户</h3>
+        </HeaderLeft>
+        <HeaderRight>
+          
+          <button
+            onClick={() => {
+              logout();
+            }}
+          >
+            登出
+          </button>
+        </HeaderRight>
+      </Header>
+      <Main>
+        <ProjectListScreen />
+      </Main>
+    </Container>
+  );
+};
+
+const Header = styled(Row)`
+`;
+const Main = styled.main`
+`;
+
+const HeaderLeft = styled(Row)`
+
+`;
+const HeaderRight = styled.div``;
+
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 6rem 1fr 6rem;
+  height: 100vh;
+`;

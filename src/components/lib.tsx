1b2bb532534979b8334777ae0ee9dca0ca2c1b32
835627827 <835627827@qq.com>
2021-07-08 17:06:10 +0800

    fix：登陆失败的处理

--- src/components/lib.tsx
+++ src/components/lib.tsx
@@ -1,4 +1,6 @@
 import styled from "@emotion/styled";
+import { Spin, Typography } from "antd";
+import { DevTools } from "jira-dev-tool";
 
 export const Row = styled.div<{
   gap?: number | boolean;
@@ -21,3 +23,18 @@ export const Row = styled.div<{
         : "undefined"};
   }
 `;
+
+const FullPage = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export const FullPageLoading = ()=>{
+  return <FullPage> <Spin size={"large"}></Spin></FullPage>
+}
+
+export const FullPageErrorFallback = ({error}:{error:Error|null}) =>{
+  return <FullPage> <Typography.Text type={"danger"}>{error?.message}</Typography.Text><DevTools/></FullPage>
+}
--- src/context/auth-context.tsx
+++ src/context/auth-context.tsx
@@ -1,8 +1,10 @@
-import React, { ReactNode, useState } from 'react'
+import React, { ReactNode } from 'react'
 import * as auth from 'auth-provider'
 import { User } from 'screens/project-list/search-panel'
 import { http } from 'utils/http'
 import { useMount } from 'utils'
+import { useAsync } from 'utils/use-async'
+import { FullPageErrorFallback, FullPageLoading } from 'components/lib'
 const AuthContext = React.createContext <{ 
     user:User|null,
     login:(from:AuthForm)=>Promise<void>
@@ -28,14 +30,21 @@ const bootstrapUser = async () =>{
 AuthContext.displayName = 'AuthContext'
 
 export const AuthProvider = ({children}:{children:ReactNode}) =>{
-    const [user,setUser] = useState <User|null> (null)
-
+    // const [user,setUser] = useState <User|null> (null)
+    const  {data:user,error,isLoading,isIdle,isError,run,setData:setUser} = useAsync<User|null>()
     const login  = (from:AuthForm)=>  auth.login(from).then (setUser)
     const register  = (from:AuthForm)=>  auth.register(from).then (setUser)
     const logout  = ()=>  auth.logout().then (() => setUser(null))
     useMount(()=>{
-        bootstrapUser().then(setUser)
-    }) 
+        run(bootstrapUser())
+    });
+    if(isIdle||isLoading){
+        return <FullPageLoading/>
+    }
+    if(isError){
+    return <FullPageErrorFallback error={error}></FullPageErrorFallback>
+        
+    }
     return <AuthContext.Provider children={children} value={{user,login,register,logout}} />    
 }
 export const useAuth = ()=>{
